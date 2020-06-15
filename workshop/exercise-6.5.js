b35fab26ca5e43d1123fe5b54820899809c74a7d
Caroline Vinet <carovinet@gmail.com>
2020-06-15 15:41:21 -0400

    exercise 6.5

--- workshop/exercise-6.5.js
+++ workshop/exercise-6.5.js
@@ -9,3 +9,13 @@
 // ...
 
 // many other possibilities here.
+
+
+for (i=0; i<=15; i++){
+    if( i % 2 === 0 ){
+        console.log (i + " is even")
+    }
+    else{
+        console.log (i + " is odd")
+    }
+}
